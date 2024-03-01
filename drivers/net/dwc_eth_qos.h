126d8af322394334785622ec53fc1a2925f6b4b8
Greg Malysa <greg.malysa@timesys.com>
2024-03-01 03:54:17 -0500

    net: Add support for ADI SC5xx SoCs with DWC QoS ethernet
    The ADI SC598 includes a Designware QoS 5.20a IP block. This
    commit adds support for using the existing ethernet QoS driver
    with the SC598 SoC.
    
    Co-developed-by: Ian Roberts <ian.roberts@timesys.com>
    Signed-off-by: Ian Roberts <ian.roberts@timesys.com>
    Co-developed-by: Nathan Barrett-Morrison <nathan.morrison@timesys.com>
    Signed-off-by: Nathan Barrett-Morrison <nathan.morrison@timesys.com>
    Signed-off-by: Vasileios Bimpikas <vasileios.bimpikas@analog.com>
    Signed-off-by: Utsav Agarwal <utsav.agarwal@analog.com>
    Signed-off-by: Arturs Artamonovs <arturs.artamonovs@analog.com>
    Signed-off-by: Greg Malysa <greg.malysa@timesys.com>

--- drivers/net/dwc_eth_qos.h
+++ drivers/net/dwc_eth_qos.h
@@ -84,6 +84,7 @@ struct eqos_mac_regs {
 #define EQOS_MAC_MDIO_ADDRESS_CR_SHIFT			8
 #define EQOS_MAC_MDIO_ADDRESS_CR_100_150		1
 #define EQOS_MAC_MDIO_ADDRESS_CR_20_35			2
+#define EQOS_MAC_MDIO_ADDRESS_CR_150_250		4
 #define EQOS_MAC_MDIO_ADDRESS_CR_250_300		5
 #define EQOS_MAC_MDIO_ADDRESS_SKAP			BIT(4)
 #define EQOS_MAC_MDIO_ADDRESS_GOC_SHIFT			2
@@ -293,3 +294,4 @@ extern struct eqos_config eqos_qcom_config;
 extern struct eqos_config eqos_stm32mp13_config;
 extern struct eqos_config eqos_stm32mp15_config;
 extern struct eqos_config eqos_jh7110_config;
+extern struct eqos_config eqos_adi_config;
