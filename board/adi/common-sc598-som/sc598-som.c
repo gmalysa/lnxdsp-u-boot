15e7137c92cb82408a3e20f798347161539bd001
Greg Malysa <malysagreg@gmail.com>
2025-04-02 23:17:35 -0400

    board: adi: Add support for SC598
    This adds support for the Analog Devices SC598-SOM and configurations
    for using it with both the SOMCRR-EZKIT and SOMCRR-EZLITE.
    
    Signed-off-by: Vasileios Bimpikas <vasileios.bimpikas@analog.com>
    Signed-off-by: Utsav Agarwal <utsav.agarwal@analog.com>
    Signed-off-by: Arturs Artamonovs <arturs.artamonovs@analog.com>
    Signed-off-by: Nathan Barrett-Morrison <nathan.morrison@timesys.com>
    Signed-off-by: Greg Malysa <malysagreg@gmail.com>

---
+++ board/adi/common-sc598-som/sc598-som.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * (C) Copyright 2022 - Analog Devices, Inc.
+ *
+ * Written and/or maintained by Timesys Corporation
+ *
+ * Contact: Nathan Barrett-Morrison <nathan.morrison@timesys.com>
+ * Contact: Greg Malysa <greg.malysa@timesys.com>
+ */
+
+#include <config.h>
+#include <phy.h>
+#include <asm/u-boot.h>
+#include <asm/arch-adi/sc5xx/sc5xx.h>
+#include <asm/arch-adi/sc5xx/soc.h>
+#include <asm/armv8/mmu.h>
+
+#include "../carriers/somcrr.h"
+
+int board_phy_config(struct phy_device *phydev)
+{
+	if (IS_ENABLED(CONFIG_ADI_CARRIER_SOMCRR_EZKIT))
+		fixup_dp83867_phy(phydev);
+	return 0;
+}
+
+int board_init(void)
+{
+	sc59x_remap_ospi();
+
+	if (IS_ENABLED(CONFIG_ADI_CARRIER_SOMCRR_EZKIT) ||
+	    IS_ENABLED(CONFIG_ADI_CARRIER_SOMCRR_EZLITE)) {
+		adi_somcrr_init_ethernet();
+	}
+
+	sc5xx_enable_rgmii();
+
+	return 0;
+}
+
